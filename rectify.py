9c6036c3fec2afed270008e06dffcf1f3a05c84e
Muhamad Rafli Susanto <73054214+raflisusanto@users.noreply.github.com>
2023-11-28 23:35:17 +0700

    Added: reorder function

--- rectify.py
+++ rectify.py
@@ -3,12 +3,54 @@ import numpy as np
 import os
 import argparse
 
+
+def order_corners(corners):
+    # Reshape corners array
+    corners_flat = corners.reshape(-1, 2)
+
+    # Calculate centroid of the corners
+    centroid = np.mean(corners_flat, axis=0)
+
+    # Calculate angles of each corner with respect to the centroid
+    angles = np.arctan2(
+        corners_flat[:, 1] - centroid[1], corners_flat[:, 0] - centroid[0]
+    )
+
+    # Order corners based on angles
+    order = np.argsort(angles)
+    sorted_corners = corners_flat[order]
+
+    # Reshape to the original format
+    sorted_corners = sorted_corners.reshape(4, 2)
+
+    # Check condition and swap if needed
+    if sorted_corners[1][0] < sorted_corners[3][0]:
+        sorted_corners[1], sorted_corners[3] = (
+            sorted_corners[3].copy(),
+            sorted_corners[1].copy(),
+        )
+
+    if sorted_corners[1][1] > sorted_corners[2][1]:
+        sorted_corners[1], sorted_corners[2] = (
+            sorted_corners[2].copy(),
+            sorted_corners[1].copy(),
+        )
+
+    if sorted_corners[0][1] > sorted_corners[3][1]:
+        sorted_corners[0], sorted_corners[3] = (
+            sorted_corners[3].copy(),
+            sorted_corners[0].copy(),
+        )
+
+    return sorted_corners
+
+
 def process_image(image_file, output_folder):
     # Try negative in the second loop if not warped
     is_warped = False
     for i in range(2):
         # Load the input image
-        image = cv2.imread(image_file)
+        image = cv2.imread(os.path.join(dir, image_file))
 
         # Image upscaling
         # Define the scaling factor (4x upscale)
@@ -38,11 +80,13 @@ def process_image(image_file, output_folder):
 
         # Apply adaptive thresholding to binarize the image
         thresh = cv2.adaptiveThreshold(
-          blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2
+            blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2
         )
 
         # Find contours in the thresholded image
-        contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours, _ = cv2.findContours(
+            thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+        )
 
         # Create a copy of the original image
         image_copy = image.copy()
@@ -70,7 +114,9 @@ def process_image(image_file, output_folder):
                     if w > 100 and h > 100:
                         # Create a mask for the detected region
                         mask = np.zeros_like(gray)
-                        cv2.drawContours(mask, [contour], -1, (255), thickness=cv2.FILLED)
+                        cv2.drawContours(
+                            mask, [contour], -1, (255), thickness=cv2.FILLED
+                        )
 
                         # Bitwise AND the mask with the original image to extract the license plate region
                         license_plate = cv2.bitwise_and(image, image, mask=mask)
@@ -86,7 +132,7 @@ def process_image(image_file, output_folder):
 
                         # Find contours in the license plate region
                         plate_contours, _ = cv2.findContours(
-                          plate_blurred, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+                            plate_blurred, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
                         )
 
                         # Iterate through the detected contours in the license plate region
@@ -96,70 +142,89 @@ def process_image(image_file, output_folder):
 
                             # Approximate the convex hull to a polygon with fewer vertices (corners)
                             plate_epsilon = 0.07 * cv2.arcLength(plate_hull, True)
-                            plate_approx = cv2.approxPolyDP(plate_hull, plate_epsilon, True)
+                            plate_approx = cv2.approxPolyDP(
+                                plate_hull, plate_epsilon, True
+                            )
 
                             # If the polygon has 4 vertices (corners), proceed to warp it
                             if len(plate_approx) == 4:
                                 # Convert the polygon to a numpy array of corners
                                 plate_corners = np.float32(plate_approx)
+                                plate_corners = order_corners(plate_corners)
 
                                 # Calculate the width and height of the detected license plate
                                 plate_width = np.linalg.norm(
-                                  plate_corners[0] - plate_corners[1]
+                                    plate_corners[0] - plate_corners[1]
                                 )
                                 plate_height = np.linalg.norm(
-                                  plate_corners[1] - plate_corners[2]
+                                    plate_corners[1] - plate_corners[2]
                                 )
 
                                 # Define the target rectangle dimensions based on the license plate width and height
-                                target_plate_width, target_plate_height = int(plate_width), int(
-                                  plate_height
-                                )
+                                target_plate_width, target_plate_height = int(
+                                    plate_width
+                                ), int(plate_height)
 
                                 # Create the target rectangle corners
                                 target_plate_corners = np.float32(
-                                  [
-                                      [0, 0],
-                                      [target_plate_width, 0],
-                                      [target_plate_width, target_plate_height],
-                                      [0, target_plate_height],
-                                  ]
+                                    [
+                                        [0, 0],
+                                        [target_plate_width, 0],
+                                        [target_plate_width, target_plate_height],
+                                        [0, target_plate_height],
+                                    ]
                                 )
 
                                 # Calculate the perspective transformation matrix for the license plate
                                 plate_M = cv2.getPerspectiveTransform(
-                                  plate_corners, target_plate_corners
+                                    plate_corners, target_plate_corners
                                 )
 
                                 # Warp the license plate to the target rectangle
                                 warped_plate = cv2.warpPerspective(
-                                  image,
-                                  plate_M,
-                                  (target_plate_width, target_plate_height),
+                                    image,
+                                    plate_M,
+                                    (target_plate_width, target_plate_height),
                                 )
 
                                 # Check if the width is shorter than the height
                                 if target_plate_width < target_plate_height:
-                                  # Rotate the image 90 degrees clockwise
-                                  warped_plate = cv2.rotate(
-                                      warped_plate, cv2.ROTATE_90_CLOCKWISE
-                                  )
+                                    # Rotate the image 90 degrees clockwise
+                                    warped_plate = cv2.rotate(
+                                        warped_plate, cv2.ROTATE_90_CLOCKWISE
+                                    )
 
                                 # Flip the image horizontally
                                 warped_plate = cv2.flip(warped_plate, 1)
 
                                 # Save the warped license plate to the output folder
-                                output_file = os.path.join(output_folder, f"rectified_{os.path.basename(image_file)}")
+                                output_file = os.path.join(
+                                    output_folder,
+                                    f"rectified_{os.path.basename(image_file)}",
+                                )
                                 cv2.imwrite(output_file, warped_plate)
                                 is_warped = True
 
         if is_warped:
             break
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input_folder', type=str, required=True, help='Input folder containing images')
-    parser.add_argument('-o', '--output_folder', type=str, default='./crop_result', help='Output folder for processed images')
+    parser.add_argument(
+        "-i",
+        "--input_folder",
+        type=str,
+        required=True,
+        help="Input folder containing images",
+    )
+    parser.add_argument(
+        "-o",
+        "--output_folder",
+        type=str,
+        default="./crop_result",
+        help="Output folder for processed images",
+    )
     args = parser.parse_args()
 
     # Create the output folder if it doesn't exist
